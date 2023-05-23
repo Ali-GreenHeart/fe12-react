38e4aff3dd97601da0b3136088f193d50acd379d
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-23 22:00:20 +0400

    remove redundant files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <div>
+    <h1>Salam from react</h1>
+    <img
+      src='https://media.istockphoto.com/id/1444486710/photo/lion-portrait.jpg?s=2048x2048&w=is&k=20&c=zPOKSJbykV_93ZU-LeMLt3ZrNGA865qm8JCPCpk_EIM=' />
+  </div>
+);
