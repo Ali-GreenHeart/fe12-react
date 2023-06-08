8c88dec119ca69e1feb518498346abd76627c50e
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-06-08 20:51:51 +0400

    add edit, remove functionality

--- src/page/Users/index.jsx
+++ src/page/Users/index.jsx
@@ -20,6 +20,25 @@ const Users = () => {
                 users.map(({ id, name, email, company }) => {
                     return (
                         <div className={styles.user} key={id}>
+                            <button
+                                onClick={() => {
+                                    const otherUsers = users.filter((elem) => elem.id !== id)
+                                    setUsers(otherUsers)
+                                }}
+                                className={styles.removeBtn}>x</button>
+                            <button
+                                onClick={() => {
+                                    const newName = prompt('yeni adi daxil edin', name)
+                                    const changedUsers = users.map((elem) => {
+                                        if (elem.id === id) {
+                                            return { ...elem, name: newName }
+                                        }
+                                        return elem;
+                                    })
+                                    setUsers(changedUsers)
+                                }}
+                                className={styles.removeBtn}>🖊</button>
+
                             <h3 className={styles.heading}>{name}</h3>
                             <p className={styles.email}>{email}</p>
                             <p className={styles.email}>-- {company.name} --</p>
